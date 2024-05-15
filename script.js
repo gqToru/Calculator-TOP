56b6795999910d780fa98a6c13beabc6185aefd9
Hector Mata <hrmb26@gmail.com>
2024-05-15 09:24:30 -0300

    se vienen cositas

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 console.log("Link successful");
 const elements = document.querySelectorAll("#numbers");
+const display = document.querySelector("#displayNumbers");
+let numberValue = [];
 
 for (let i = 0; i < elements.length; i++) {
   elements[i].addEventListener("click", function () {
-    const numberValue = this.value;
+    numberValue.push(this.value);
+    firstOperator;
+    display.textContent = numberValue.join("");
     console.log(numberValue);
   });
 }
