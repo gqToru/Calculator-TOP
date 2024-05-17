13357ef1bdfefed98b0574248cdcc61a5303cac3
Hector Mata <hrmb26@gmail.com>
2024-05-17 08:52:48 -0300

    clear button

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 console.log("Link successful");
 const elements = document.querySelectorAll("#numbers");
 const display = document.querySelector("#displayNumbers");
+const clearBtn = document.querySelector("#clearBtn");
 let numberValue = [];
+let joinedValue = 0;
 
 for (let i = 0; i < elements.length; i++) {
   elements[i].addEventListener("click", function () {
     numberValue.push(this.value);
-    firstOperator;
-    display.textContent = numberValue.join("");
-    console.log(numberValue);
+    joinedValue = parseInt(numberValue.join(""));
+    display.textContent = joinedValue;
+    console.log(joinedValue);
   });
 }
+
+clearBtn.addEventListener("click", function () {
+  display.textContent = "0";
+  joinedValue = 0;
+  numberValue = [];
+});
